b6e09ed762e6d818e2f7a425346113fc126ddeff
Jessé das Neves <jesse.neves@omnismart.com.br>
2026-07-05 15:29:31 -0300

    fix: use generate getter and setter with lombok

--- lesson/src/main/java/com/studygenie/lesson/contentgenerator/ContentGeneratorProperties.java
+++ lesson/src/main/java/com/studygenie/lesson/contentgenerator/ContentGeneratorProperties.java
@@ -2,7 +2,12 @@ package com.studygenie.lesson.contentgenerator;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @ConfigurationProperties(prefix = "content-generator")
+@Setter
+@Getter
 public class ContentGeneratorProperties {
 
     private String authUrl;
@@ -11,52 +16,4 @@ public class ContentGeneratorProperties {
     private String secret;
     private String tenantName;
     private int timeoutSeconds = 20;
-
-    public String getAuthUrl() {
-        return authUrl;
-    }
-
-    public void setAuthUrl(String authUrl) {
-        this.authUrl = authUrl;
-    }
-
-    public String getCompletionsUrl() {
-        return completionsUrl;
-    }
-
-    public void setCompletionsUrl(String completionsUrl) {
-        this.completionsUrl = completionsUrl;
-    }
-
-    public String getAccessKey() {
-        return accessKey;
-    }
-
-    public void setAccessKey(String accessKey) {
-        this.accessKey = accessKey;
-    }
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
-    public String getTenantName() {
-        return tenantName;
-    }
-
-    public void setTenantName(String tenantName) {
-        this.tenantName = tenantName;
-    }
-
-    public int getTimeoutSeconds() {
-        return timeoutSeconds;
-    }
-
-    public void setTimeoutSeconds(int timeoutSeconds) {
-        this.timeoutSeconds = timeoutSeconds;
-    }
 }
