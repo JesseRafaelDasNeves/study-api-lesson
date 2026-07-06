ab2ff5fc8e97df33316615bcb4c9a960499cba1f
Jessé das Neves <jesse.neves@omnismart.com.br>
2026-07-06 17:23:00 -0300

    fix: response end point getAll lesson and get lesson with object course

--- lesson/src/main/java/com/studygenie/lesson/lesson/LessonResponseDTO.java
+++ lesson/src/main/java/com/studygenie/lesson/lesson/LessonResponseDTO.java
@@ -2,23 +2,26 @@ package com.studygenie.lesson.lesson;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import com.studygenie.lesson.course.CourseResponseDTO;
 
 public record LessonResponseDTO(
         UUID id,
-        UUID courseId,
+        CourseResponseDTO course,
         String title,
         String description,
         LocalDateTime date,
+        Boolean hasSummary,
         LocalDateTime createdAt,
         LocalDateTime updatedAt) {
 
     public static LessonResponseDTO from(Lesson lesson) {
         return new LessonResponseDTO(
                 lesson.getId(),
-                lesson.getCourse().getId(),
+                lesson.getCourse() != null ? CourseResponseDTO.from(lesson.getCourse()) : null,
                 lesson.getTitle(),
                 lesson.getDescription(),
                 lesson.getDate(),
+                lesson.getSummary() != null,
                 lesson.getCreatedAt(),
                 lesson.getUpdatedAt());
     }
