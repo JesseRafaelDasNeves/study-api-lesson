10c33b5c45579bb5afe3ab26db96618bfc7c64f9
Jessé das Neves <jesse.neves@omnismart.com.br>
2026-07-06 07:31:12 -0300

    fix: add trait cours

--- lesson/src/main/java/com/studygenie/lesson/LessonApplication.java
+++ lesson/src/main/java/com/studygenie/lesson/LessonApplication.java
@@ -3,6 +3,9 @@ package com.studygenie.lesson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -10,6 +13,19 @@ public class LessonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LessonApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
